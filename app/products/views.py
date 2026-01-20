f630a3c61ea7fcf4f88f3ed101b64c916f7d1356
Serdar Güneş <serdargunes@users.noreply.github.com>
2026-01-20 13:01:43 +0300

    Add Product model to admin panel

--- app/products/views.py
+++ app/products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Product
+from django.http import JsonResponse
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    data = {
+    'products' : list(products.values())
+    }
+    return JsonResponse(data)
+
+def product_details(request,pk):
+    product=Product.objects.get(pk=pk)
+    data = {
+        'name': product.name,
+        'description': product.description,
+        'price': product.price,
+        'stock' : product.stock,
+    }
+    return JsonResponse(data)
