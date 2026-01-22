8327e381d20f5399d661cb69c3a34f49c8a5070d
Serdar Güneş <serdargunes@users.noreply.github.com>
2026-01-22 05:23:24 +0300

    serializers

--- app/products/models.py
+++ app/products/models.py
@@ -8,3 +8,5 @@ class Product(models.Model):
   
     def __str__(self):
         return self.name
+
+
--- app/products/views.py
+++ app/products/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
 from .models import Product
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from .serializers import ProductSerializer
+from rest_framework.response import Response
 
+@api_view()
 def product_list(request):
-    products = Product.objects.all()
-    data = {
-    'products' : list(products.values())
-    }
-    return JsonResponse(data)
+    products =Product.objects.all()
+    serializer = ProductSerializer(products,many=True)
+    return Response(serializer.data)
 
+@api_view()
 def product_details(request,pk):
-    product=Product.objects.get(pk=pk)
-    data = {
-        'name': product.name,
-        'description': product.description,
-        'price': product.price,
-        'stock' : product.stock,
-    }
-    return JsonResponse(data)
+    product = Product.objects.get(pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
