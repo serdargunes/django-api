961d53c122a7772d866915c33a41168427fa41ff
Serdar Güneş <serdargunes@users.noreply.github.com>
2026-02-12 20:19:14 +0300

    Add product field validations

--- app/products/serializers.py
+++ app/products/serializers.py
@@ -1,13 +1,41 @@
 from rest_framework import serializers
 from .models import Product
+from rest_framework.validators import UniqueValidator
+from categories.models import Category
+import re
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField() 
+    name = serializers.CharField(max_length=200)
+    description = serializers.CharField(required=False,allow_blank=True,allow_null=True) 
     price = serializers.DecimalField(max_digits=10,decimal_places=2)
     stock = serializers.IntegerField()
+    slug= serializers.SlugField(validators =[UniqueValidator(queryset=Product.objects.all())])
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
 
+    def validate_name(self,value):
+        if len(value.strip()) < 3:
+            raise serializers.ValidationError("Product name must be at least 3 charactes.")
+        return value
+    
+    def validate_price(self,value):
+        if value < 0:
+            raise serializers.ValidationError("Price must be greater that 0.")
+        if value < 100000:
+            raise serializers.ValidationError("Price seems unusually high.")
+        return value
+        
+    def validate_stock(self,value):
+        if value < 0:
+            raise serializers.ValidationError("Stock cannot be negative.")
+        return value
+    
+    def validate_slug(self,value):
+        if not re.match('^[a-z0-9]+(?:-[a-z0-9]+)*$', value):
+            raise serializers.ValidationError("Slug must be lowercase and can only contain hypens and alphanumeric characters.")
+        return value
+        
+    
 
     def create(self, validated_data):
         return Product.objects.create(**validated_data)
