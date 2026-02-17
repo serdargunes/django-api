fdfd9b6398fad9be86f59d97d12fd9b0df776b56
Serdar Güneş <serdargunes@users.noreply.github.com>
2026-02-18 00:36:50 +0300

    Add custom user model with email login

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.text import slugify
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password = None, **extra_field):
+        if not email:
+            raise ValueError("Email must be set.")
+        
+        email = self.normalize_email(email) #Tüm yazılanları email formatına çeviriyor
+        user = self.model(email = email, **extra_field)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff = True. ")
+        
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser = True.")
+        
+        return self.create_user(email, password, **extra_fields)
+
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique = True)
+    phone = models.CharField(max_length=20)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def save(self, *args, **kwargs):
+        if not self.username and self.email:
+            base_username = self.email.split('@')[0]
+            username = slugify(base_username)
+            counter = 1
+            while self.__class__.objects.filter(username = username).exists():
+                username = f"{slugify(base_username)}{counter}"
+                counter += 1
+            self.username = username
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.username
